ba5fed7266e12652cfaa559033eb10d6760bfc7f
Thiago Soares <thiago.soares@estantemagica.com.br>
2023-04-10 08:18:39 -0400

    feat: add product interface

--- src/domain/product/entity/product.ts
+++ src/domain/product/entity/product.ts
@@ -1,5 +1,6 @@
+import ProductInterface from "./product.interface";
 
-export default class Product{
+export default class Product implements ProductInterface{
   private _id: string;
   private _name: string;
   private _price: number;
