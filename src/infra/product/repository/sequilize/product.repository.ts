ef083f976986f718d62d204cbcd056bf9dd6dd6e
Thiago Soares <thiago.soares@estantemagica.com.br>
2023-04-11 08:33:04 -0400

    fix: add product interface

--- src/infra/product/repository/sequilize/product.repository.ts
+++ src/infra/product/repository/sequilize/product.repository.ts
@@ -1,31 +1,44 @@
 import Product from "../../../../domain/product/entity/product";
+import ProductInterface from "../../../../domain/product/entity/product.interface";
 import ProductRepositoryInterface from "../../../../domain/product/repository/product.repository.interface";
 import ProductModel from "./product.model";
 
 
 export default class ProductRepository implements ProductRepositoryInterface {
-    async create(entity: Product): Promise<void> {
+    async create(entity: ProductInterface): Promise<void> {
         await ProductModel.create({
             id: entity.id,
             name: entity.name,
             price: entity.price
         });
     }
-    async update(entity: Product): Promise<void> {
+    async update(entity: ProductInterface): Promise<void> {
         await ProductModel.update({
             name: entity.name,
             price: entity.price
-            }, 
-            {where: {id: entity.id}
-        })
+        },
+            {
+                where: { id: entity.id }
+            })
     }
-    async find(id: string): Promise<Product> {
-        const product = await ProductModel.findOne({where: {id: id}});
-        return new Product(product.id, product.name, product.price)
+    async find(id: string): Promise<ProductInterface> {
+        let productModel;
+        try {
+            productModel = await ProductModel.findOne(
+                { 
+                    where: { id: id } ,
+                    rejectOnEmpty: true,
+                }
+            );
+            console.log(productModel);
+        } catch (error) {
+            throw new Error("Product not found");
+          }
+        return new Product(productModel.id, productModel.name, productModel.price)
     }
-    async findAll(): Promise<Product[]> {
-       const products = await ProductModel.findAll();
-       return products.map(
+    async findAll(): Promise<ProductInterface[]> {
+        const products = await ProductModel.findAll();
+        return products.map(
             product => new Product(product.id, product.name, product.price)
         );
     }
