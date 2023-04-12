05ae5435124875db3e741ba52ebf79a35cb7e402
Thiago Soares <thiago.soares@estantemagica.com.br>
2023-04-12 08:53:44 -0400

    feat: config server

---
+++ src/infra/api/express.ts
@@ -0,0 +1,26 @@
+import express, { Express } from 'express';
+import { Sequelize } from 'sequelize-typescript';
+import CustomerModel from '../customer/repository/sequilize/customer.model';
+import ProductModel from '../product/repository/sequilize/product.model';
+import { customerRoute } from './routes/customer.route';
+import { productRoute } from './routes/product.route';
+
+export const app: Express = express();
+app.use(express.json());
+app.use("/customer", customerRoute)
+app.use("/product", productRoute)
+
+export let sequelize: Sequelize;
+
+async function start() {
+  sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: ':memory:',
+    logging: false,
+  });
+
+  await sequelize.addModels([CustomerModel, ProductModel]);
+  await sequelize.sync();
+}
+
+start();
